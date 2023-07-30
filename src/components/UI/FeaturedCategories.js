4f55643c081f3a9b933168229671f6cee52ca242
saharabanu <saharabanungn@gmail.com>
2023-07-30 11:22:00 +0600

    [DONE] product categories

--- src/components/UI/FeaturedCategories.js
+++ src/components/UI/FeaturedCategories.js
@@ -1,26 +1,70 @@
-import { Col, Row } from "antd"
+import { Col, Row } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+
+const categories = [
+  {
+    title: "CPU",
+    image: '/images/cpu.png',
+    href: "/categories/cpu",
+  },
+  {
+    title: "Motherboard",
+    image: '/images/motherboard.png',
+    href: "/categories/motherboard",
+  },
+  {
+    title: "RAM",
+    image: '/images/ram.png',
+    href: "/categories/ram",
+  },
+  {
+    title: "Power Supply Unit",
+    image: '/images/power.png',
+    href: "/categories/powersupply",
+  },
+  {
+    title: "Storage Device",
+    image: '/images/storage.png',
+    href: "/categories/storagedevice",
+  },
+  {
+    title: "Monitor",
+    image: '/images/monitor.png',
+    href: "/categories/monitor",
+  },
+  {
+    title: "Others",
+    image: '/images/others.png',
+    href: "/categories/others",
+  },
+];
 
 
 const FeaturedCategories = () => {
   return (
     <div className="container-fluid">
 
-<div style={{display:"flex", justifyContent:'center', alignItems:"center", padding:'10px 0px', fontWeight:'bold'}}>
+<div style={{textAlign:"center",padding:"40px 10px"}}>
 <h2>Featured Categories</h2>
     </div>
+
+
     <Row gutter={[8, 8]}>
-  <Col xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 8 }} xl={{span:4}} >
-  <div style={{background:"#fff",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}>
-          <h5 style={{textAlign:"center",padding:"40px 10px"}}> Categories</h5>
-  </div>
+
+    {categories?.map((category) => (<Col key={category?.title} xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 8 }} xl={{span:4}} >
+          <Link href={category?.href}>
+          <div style={{background:"#fff",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", padding:'30px  ', borderRadius: "10px"}}>
+          <Image width={50}  height={50} src={category?.image} alt="category-img"></Image>
+          <p> {category?.title}</p>
+          </div>
+          </Link>
+  
   
   </Col>
-  <Col xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 8 }} xl={{span:4}} >
-  <div style={{background:"#fff",boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}>
-          <h5 style={{textAlign:"center",padding:"40px 10px"}}> Categories</h5>
-  </div>
+    ))}
   
-  </Col>
+  
   
 </Row>
 
